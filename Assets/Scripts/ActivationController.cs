add495f6837461802118091524aa8e76ef682fe9
sweisian <rswei888@gmail.com>
2019-05-06 01:08:28 -0500

    still working on grouping objects

--- Assets/Scripts/ActivationController.cs
+++ Assets/Scripts/ActivationController.cs
@@ -10,19 +10,19 @@ public class ActivationController : MonoBehaviour
 
     private void OnValidate()
     {
-        //Renderer rend = gameObject.GetComponent<Renderer>();
+        Renderer rend = gameObject.GetComponent<Renderer>();
 
-        //if (rend)
-        //{
-        //    ColorSwitcher(rend, myGroup);
-        //}
-        //else
-        //{
-        //    foreach (Renderer childRend in gameObject.GetComponentsInChildren<Renderer>())
-        //    {
-        //        ColorSwitcher(childRend, myGroup);
-        //    }
-        //}
+        if (rend)
+        {
+            ColorSwitcher(rend, myGroup);
+        }
+        else
+        {
+            foreach (Renderer childRend in gameObject.GetComponentsInChildren<Renderer>())
+            {
+                ColorSwitcher(childRend, myGroup);
+            }
+        }
     }
 
     private void ColorSwitcher(Renderer rend, Group thisGroup)
@@ -30,19 +30,34 @@ public class ActivationController : MonoBehaviour
         switch (myGroup)
         {
             case ActivationController.Group.Zero:
-                rend.sharedMaterial.color = Color.magenta;
+                rend.material.color = Color.magenta;
                 break;
             case ActivationController.Group.One:
-                rend.sharedMaterial.color = Color.blue;
+                rend.material.color = Color.blue;
                 break;
             case ActivationController.Group.Two:
-                rend.sharedMaterial.color = Color.cyan;
+                rend.material.color = Color.cyan;
                 break;
             case ActivationController.Group.Three:
-                rend.sharedMaterial.color = Color.green;
+                rend.material.color = Color.green;
                 break;
             case ActivationController.Group.Four:
-                rend.sharedMaterial.color = Color.yellow;
+                rend.material.color = Color.yellow;
+                break;
+            case ActivationController.Group.Five:
+                rend.material.color = Color.grey;
+                break;
+            case ActivationController.Group.Six:
+                rend.material.color = Color.red;
+                break;
+            case ActivationController.Group.Seven:
+                rend.material.color = Color.black;
+                break;
+            case ActivationController.Group.Eight:
+                rend.material.color = Color.white;
+                break;
+            case ActivationController.Group.Nine:
+                rend.material.color = new Color32(139, 69, 19, 255);
                 break;
         }
     }
